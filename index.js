68a9c928a6623cd4f1cd6b2fa799c44f267fe7ac
Xiejvar <60881169+Xiejvar@users.noreply.github.com>
2020-03-11 00:40:38 +0100

    cambios de html a js

---
+++ index.js
@@ -0,0 +1,87 @@
+
+
+const cuerpo = document.getElementById("cuerpo");
+
+cuerpo.innerHTML = `
+<section>
+<header>
+    <h2 class="letra">Welcome to the</h2>
+    <h1 class="letra sombra">QUIZ!!!</h1>
+</header>
+</section>
+<section>
+<article>
+    <h3>Pregunta 1</h3>
+</article>
+<article class="columna">
+    <label for="resp1pre1">Respuesta 1</label>
+    <input type="radio" name="resppre1" id="resp1pre1">
+    <label for="resp2pre1">Respuesta 2</label>
+    <input type="radio" name="resppre1" id="resp2pre1">
+    <label for="resp3pre1">Respuesta 3</label>
+    <input type="radio" name="resppre1" id="resp3pre1">
+    <label for="resp4pre1">Respuesta 4</label>
+    <input type="radio" name="resppre1" id="resp4pre1">
+</article>
+</section>
+<section>
+<article>
+    <h3>Pregunta 2<h3>
+</article>
+<article class="columna">
+    <label for="resp1pre2">Respuesta 1</label>
+    <input type="radio" name="resppre2" id="resp1pre2">
+    <label for="resp2pre2">Respuesta 2</label>
+    <input type="radio" name="resppre2" id="resp2pre2">
+    <label for="resp3pre2">Respuesta 3</label>
+    <input type="radio" name="resppre2" id="resp3pre2">
+    <label for="resp4pre2">Respuesta 4</label>
+    <input type="radio" name="resppre2" id="resp4pre2">
+</article>
+</section>
+<section>
+<article>
+    <h3>Pregunta 3</h3>
+</article>
+<article class="columna">
+    <label for="resp1pre3">Respuesta 1</label>
+    <input type="radio" name="resppre3" id="resp1pre3">
+    <label for="resp2pre3">Respuesta 2</label>
+    <input type="radio" name="resppre3" id="resp2pre3">
+    <label for="resp3pre3">Respuesta 3</label>
+    <input type="radio" name="resppre3" id="resp3pre3">
+    <label for="resp4pre3">Respuesta 4</label>
+    <input type="radio" name="resppre3" id="resp4pre3">
+</article>
+</section>
+<section>
+<article>
+    <h3>Pregunta 4</h3>
+</article>
+<article class="columna">
+    <label for="resp1pre4">Respuesta 1</label>
+    <input type="radio" name="resppre4" id="resp1pre4">
+    <label for="resp2pre4">Respuesta 2</label>
+    <input type="radio" name="resppre4" id="resp2pre4">
+    <label for="resp3pre4">Respuesta 3</label>
+    <input type="radio" name="resppre4" id="resp3pre4">
+    <label for="resp4pre4">Respuesta 4</label>
+    <input type="radio" name="resppre4" id="resp4pre4">
+</article>
+</section>
+<section>
+<article>
+    <h3>Pregunta 5</h3>
+</article>
+<article class="columna">
+    <label for="resp1pre5">Respuesta 1</label>
+    <input type="radio" name="resp1pre5" id="resp1pre5">
+    <label for="resp2pre5">Respuesta 2</label>
+    <input type="radio" name="resp2pre5" id="resp2pre5">
+    <label for="resp3pre5">Respuesta 3</label>
+    <input type="radio" name="resp3pre5" id="resp3pre5">
+    <label for="resp4pre5">Respuesta 4</label>
+    <input type="radio" name="resp4pre5" id="resp4pre5">
+</article>
+</section>
+`;
