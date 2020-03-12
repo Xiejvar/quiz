bd3c2e880e8e04268560822ff05c2fa39f6a6a58
Xiejvar <60881169+Xiejvar@users.noreply.github.com>
2020-03-12 22:16:01 +0100

    mejoras

--- index.js
+++ index.js
@@ -1,140 +1,203 @@
 
 const pregunta1 = {
-    titulo : "Como llaman a Gandalf en el Oeste?",
-    respuestas : [
-        "Glorfindel",
-        "Aerendil",
-        "Mithrandir",
-        "Maegir"
+    titulo: "Como llaman a Gandalf en el Oeste?",
+    respuestas: [{
+            label: "Glorfindel",
+            id: "Glorfindel",
+            name: "pre1",
+            value: "Glorfindel"
+        },
+        {
+            label: "Aerendil",
+            id: "Aerendil",
+            name: "pre1",
+            value: "Aerendil"
+        },
+        {
+            label: "Mithrandir",
+            id: "Mithrandir",
+            name: "pre1",
+            value: "Mithrandir"
+        },
+        {
+            label: "Maegir",
+            id: "Maegir",
+            name: "pre1",
+            value: "Maegir"
+        }
     ]
 }
 
 
 const pregunta2 = {
-    titulo : "Quien gano torneo de Mortal Kombat?",
-    respuestas : [
-        "Raiden",
-        "Sonia Blade",
-        "Liu-Kang",
-        "Johny Cage"
+    titulo: "Quien gano torneo de Mortal Kombat?",
+    respuestas: [{
+            label: "Raiden",
+            id: "Raiden",
+            name: "pre2",
+            value: "Raiden"
+        },
+        {
+            label: "Sonia Blade",
+            id: "SoniaBlade",
+            name: "pre2",
+            value: "SoniaBlade"
+        },
+        {
+            label: "Liu-Kang",
+            id: "LiuKang",
+            name: "pre2",
+            value: "LiuKang"
+        },
+        {
+            label: "Johny Cage",
+            id: "JohnyCage",
+            name: "pre2",
+            value: "JohnyCage"
+        }
     ]
 }
 
 
 const pregunta3 = {
-    titulo : "En que Resident Evil ocurre la destruccion de Racoon City",
-    respuestas : [
-        "RE1",
-        "RE3",
-        "RE4",
-        "RE2"
+    titulo: "En que Resident Evil ocurre la destruccion de Racoon City",
+    respuestas: [{
+            label: "RE1",
+            id: "RE1",
+            name: "pre3",
+            value: "RE1"
+        },
+        {
+            label: "RE3",
+            id: "RE3",
+            name: "pre3",
+            value: "RE3"
+        },
+        {
+            label: "RE4",
+            id: "RE4",
+            name: "pre3",
+            value: "RE4"
+        },
+        {
+            label: "RE2",
+            id: "RE2",
+            name: "pre3",
+            value: "RE2"
+        }
     ]
 }
 
 
 const pregunta4 = {
-    titulo : "Cuantas colas tiene el bijuu que hay dentro de Naruto",
-    respuestas : [
-        "9 colas",
-        "4 colas",
-        "7 colas",
-        "3 colas"
+    titulo: "Cuantas colas tiene el bijuu que hay dentro de Naruto",
+    respuestas: [{
+            label: "9 colas",
+            id: "9colas",
+            name: "pre4",
+            value: "9colas"
+        },
+        {
+            label: "4 colas",
+            id: "4colas",
+            name: "pre4",
+            value: "4colas"
+        },
+        {
+            label: "7 colas",
+            id: "7colas",
+            name: "pre4",
+            value: "7colas"
+        },
+        {
+            label: "3 colas",
+            id: "3colas",
+            name: "pre4",
+            value: "3colas"
+        }
     ]
 }
 
 
 const pregunta5 = {
-    titulo : "En que saga ocurrio la primera fusion entre goku y vegeta?",
-    respuestas : [
-        "Saga de Cell",
-        "Saga de los Sayan",
-        "Saga de Freezer",
-        "Saga de Bu"
+    titulo: "En que saga ocurrio la primera fusion entre goku y vegeta?",
+    respuestas: [{
+            label: "Saga de Cell",
+            id: "SagaCell",
+            name: "pre5",
+            value: "SagaCell"
+        },
+        {
+            label: "Saga de los sayan",
+            id: "SagaSayan",
+            name: "pre5",
+            value: "SagaSayan"
+        },
+        {
+            label: "Saga de Freezer",
+            id: "SagaFreezer",
+            name: "pre5",
+            value: "SagaFreezer"
+        },
+        {
+            label: "Saga de Bu",
+            id: "SagaBu",
+            name: "pre5",
+            value: "SagaBu"
+        }
     ]
 }
-const cuerpo = document.getElementById("cuerpo");
 
-cuerpo.innerHTML = `
-<section>
-<header>
-    <h2 class="letra espacio">Welcome to the</h2>
-    <h1 class="letra sombra espacio">QUIZ!!!</h1>
-</header>
-</section>
-<section class="respuesta">
-<article>
-    <h3>${pregunta1.titulo}</h3>
-</article>
-<article class="columna">
-    <label for="resp1pre1">${pregunta1.respuestas[0]}</label>
-    <input type="radio" name="resppre1" id="resp1pre1" class="esconder">
-    <label for="resp2pre1">${pregunta1.respuestas[1]}</label>
-    <input type="radio" name="resppre1" id="resp2pre1" class="esconder">
-    <label for="resp3pre1">${pregunta1.respuestas[2]}</label>
-    <input type="radio" name="resppre1" id="resp3pre1" class="esconder">
-    <label for="resp4pre1">${pregunta1.respuestas[3]}</label>
-    <input type="radio" name="resppre1" id="resp4pre1" class="esconder">
-</article>
-</section>
-<section class="respuesta">
-<article>
-    <h3>${pregunta2.titulo}<h3>
-</article>
-<article class="columna">
-    <label for="resp1pre2">${pregunta2.respuestas[0]}</label>
-    <input type="radio" name="resppre2" id="resp1pre2" class="esconder">
-    <label for="resp2pre2">${pregunta2.respuestas[1]}</label>
-    <input type="radio" name="resppre2" id="resp2pre2" class="esconder">
-    <label for="resp3pre2">${pregunta2.respuestas[2]}</label>
-    <input type="radio" name="resppre2" id="resp3pre2" class="esconder">
-    <label for="resp4pre2">${pregunta2.respuestas[3]}</label>
-    <input type="radio" name="resppre2" id="resp4pre2" class="esconder">
-</article>
-</section>
-<section class="respuesta">
-<article>
-    <h3>${pregunta3.titulo}</h3>
-</article>
-<article class="columna">
-    <label for="resp1pre3">${pregunta3.respuestas[0]}</label>
-    <input type="radio" name="resppre3" id="resp1pre3" class="esconder">
-    <label for="resp2pre3">${pregunta3.respuestas[1]}</label>
-    <input type="radio" name="resppre3" id="resp2pre3" class="esconder">
-    <label for="resp3pre3">${pregunta3.respuestas[2]}</label>
-    <input type="radio" name="resppre3" id="resp3pre3" class="esconder">
-    <label for="resp4pre3">${pregunta3.respuestas[3]}</label>
-    <input type="radio" name="resppre3" id="resp4pre3" class="esconder">
-</article>
-</section>
-<section class="respuesta">
-<article>
-    <h3>${pregunta4.titulo}</h3>
-</article>
-<article class="columna">
-    <label for="resp1pre4">${pregunta4.respuestas[0]}</label>
-    <input type="radio" name="resppre4" id="resp1pre4" class="esconder">
-    <label for="resp2pre4">${pregunta4.respuestas[1]}</label>
-    <input type="radio" name="resppre4" id="resp2pre4" class="esconder">
-    <label for="resp3pre4">${pregunta4.respuestas[2]}</label>
-    <input type="radio" name="resppre4" id="resp3pre4" class="esconder">
-    <label for="resp4pre4">${pregunta4.respuestas[3]}</label>
-    <input type="radio" name="resppre4" id="resp4pre4" class="esconder">
-</article>
-</section>
-<section class="respuesta">
-<article>
-    <h3>${pregunta5.titulo}</h3>
-</article>
-<article class="columna">
-    <label for="resp1pre5">${pregunta5.respuestas[0]}</label>
-    <input type="radio" name="resp1pre5" id="resp1pre5" class="esconder">
-    <label for="resp2pre5">${pregunta5.respuestas[1]}</label>
-    <input type="radio" name="resp2pre5" id="resp2pre5" class="esconder">
-    <label for="resp3pre5">${pregunta5.respuestas[2]}</label>
-    <input type="radio" name="resp3pre5" id="resp3pre5" class="esconder">
-    <label for="resp4pre5">${pregunta5.respuestas[3]}</label>
-    <input type="radio" name="resp4pre5" id="resp4pre5" class="esconder">
-</article>
-</section>
-`;
+const imprimeRespuesta = (respuesta) => {
+    const {
+        id,
+        label,
+        name,
+        value
+    } = respuesta
+    return `
+   <article>
+    <label for="${id}">${label}</label>
+    <input type="radio" id="${id}" name="${name}" value="${value}">
+  </article>`;
+}
 
+
+const botonazo = () => `<button>Enviar</button>`;
+
+
+const imprimeTitulo = (titulo) => {
+    return `<p>${titulo}</p>`;
+}
+
+const imprimePregunta = (pregunta) => {
+    const {
+        titulo,
+        respuestas
+    } = pregunta;
+    const iterar = respuestas.map(imprimeRespuesta);
+    const juntar = iterar.join("");
+    const titulos = imprimeTitulo(titulo);
+    return titulos + juntar;
+}
+const preguntas = [pregunta1, pregunta2, pregunta3, pregunta4, pregunta5];
+
+const preguntaImpresa = preguntas.map(imprimePregunta).join('')
+
+
+
+const imprimeRespuestas = (event) => {
+    event.preventDefault()
+
+    console.log($form.elements.pre1.value)
+    console.log($form.elements.pre2.value)
+    console.log($form.elements.pre3.value)
+    console.log($form.elements.pre4.value)
+    console.log($form.elements.pre5.value)
+}
+
+const $form = document.getElementsByTagName('form')[0];
+const boton = botonazo();
+
+$form.innerHTML = preguntaImpresa + boton;
+$form.addEventListener('submit', imprimeRespuestas);
